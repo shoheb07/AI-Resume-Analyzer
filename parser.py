126459e6010983b4877df08a11a662012f869c0b
shoheb mulla <149918582+shoheb07@users.noreply.github.com>
2026-03-28 18:58:06 +0530

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,16 @@
+import docx
+from pdfminer.high_level import extract_text
+
+def extract_text_from_pdf(file_path):
+    return extract_text(file_path)
+
+def extract_text_from_docx(file_path):
+    doc = docx.Document(file_path)
+    return "\n".join([para.text for para in doc.paragraphs])
+
+def extract_text(file_path):
+    if file_path.endswith(".pdf"):
+        return extract_text_from_pdf(file_path)
+    elif file_path.endswith(".docx"):
+        return extract_text_from_docx(file_path)
+    return ""
